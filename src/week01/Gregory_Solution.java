6baadbad2ab09f70c1a71f8ab7aba78ed6eec9ed
McGeenie <89535011+McGeenie@users.noreply.github.com>
2023-05-29 20:24:21 -0400

    Exercise prompt were written

--- src/week01/Gregory_Solution.java
+++ src/week01/Gregory_Solution.java
@@ -8,3 +8,21 @@ public class Gregory_Solution {
     }
 
 }
+/*
+1.
+Write a method which can identify if a given number is even or odd
+EX:
+    identify(5) -> "Odd"
+    identify(6) -> :Even"
+
+2.
+Numbers -- Divide without / operator
+
+Write a method that can divide two numbers without using the division operator
+
+3.
+FINRA:
+Write a method which prints out the numbers from 1 to 30 but for numbers which are multiple of 3 print "FIN" instead
+of the number and for number which area multiple of 5, print "RA" instead of the number. For numbers which are
+multiple of both 3 and 5, print "FINRA" instead of the number.
+ */
