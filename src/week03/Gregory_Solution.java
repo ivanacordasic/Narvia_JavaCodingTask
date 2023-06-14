90d7c2ad53a46a8ddb08bb4c5ec6cac13b7ae8fc
McGeenie <89535011+McGeenie@users.noreply.github.com>
2023-06-13 22:33:10 -0400

    Week3 Gregory_Solution is completed

--- src/week03/Gregory_Solution.java
+++ src/week03/Gregory_Solution.java
@@ -1,4 +1,54 @@
 package week03;
 
 public class Gregory_Solution {
+
+    public static void main(String[] args) {
+
+        primeNumber(43);
+
+        System.out.println("-----------------------------------");
+
+        reverseNegativeNumbers(-67);
+    }
+
+    public static void primeNumber(int num) {
+/*
+1. Numbers -- Prime Number
+Write a method that can check if a number is
+prime or not
+ */
+        int count = 0;
+
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                count++;
+            }
+        }
+
+        if (count == 0) {
+            System.out.println(num + " is a prime number");
+        } else {
+            System.out.println(num + " is not a prime number");
+        }
+
+    }
+
+    public static void reverseNegativeNumbers(int nums) {
+/*
+2. Numbers -- Reverse negative number
+Write a return method that can reverse
+negative number and return it as int
+ */
+
+        if (nums > 0){
+            throw new ArithmeticException("The number has to be negative");
+        }
+
+        int positiveNums = 0;
+if (nums < 0){
+    positiveNums = nums * -1;
+}
+        System.out.println(positiveNums);
+    }
+
 }
