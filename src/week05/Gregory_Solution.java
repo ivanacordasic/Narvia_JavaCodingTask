6005ab3320773bb4e4aa4cf3983753ee20d77ebb
McGeenie <89535011+McGeenie@users.noreply.github.com>
2023-06-29 11:48:18 -0400

    Week5 Gregory_Solution is created

---
+++ src/week05/Gregory_Solution.java
@@ -0,0 +1,25 @@
+package week05;
+
+public class Gregory_Solution {
+
+    public static void main(String[] args) {
+
+
+
+    }
+    /*
+    String -- Find the unique
+Write a return method that can find the unique characters from the String
+Ex: unique("AAABBBCCCDEF") ==> "DEF";
+     */
+
+    public static void unique (String Str){
+
+        int count;
+
+        for (int i = 0; i < Str.length(); i++) {
+
+        }
+
+    }
+}
